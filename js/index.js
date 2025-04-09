658055e2499dfd148494ba243e8dd7073d54b0cc
cutesori <sori2230@naver.com>
2025-04-09 17:50:00 +0900

    20250409

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //슬라이더
     function slide(){
         $('.slide').stop().animate({marginLeft:"-150%"},800,function(){
             $('.slide li:first').appendTo('.slide');
@@ -7,4 +8,9 @@ $(document).ready(function(){
     }
 
     setInterval(slide,3000);    
+
+    // 왼쪽 스티키 높이
+    $(".container_side div:nth-of-type(1)").height($(".container_left").height());
+    $(".container_side div:nth-of-type(2)").height($(".contentsecond").height());
+    $(".container_side div:nth-of-type(3)").height($(".contenttrd").height());
 });
